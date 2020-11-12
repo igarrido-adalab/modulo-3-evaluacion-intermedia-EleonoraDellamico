9d07ee6660a39ba511fda8e0b8553a913d21501f
Eleonora Dell'Amico <eleonoradellamico4@gmail.com>
2020-11-12 09:03:11 +0100

    add structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
